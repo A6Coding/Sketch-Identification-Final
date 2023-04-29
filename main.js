e783b73874ad58a01349507cb075768081f903b7
A6Coding <111938126+A6Coding@users.noreply.github.com>
2023-04-28 20:29:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function preload() {
+    classifier = ml5.imageClassifier("DoodleNet");
+}
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white"); //makes canvas white
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function clearCanvas() {
+    background("white");
+
+}
+
+function draw() {
+    strokeWeight(13);
+    stroke("black");
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        consolele.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("sketch").innerHTML = "Sketch: " + results[0].label;
+        document.getElementById("confidence").innerHTML = "Confidence: " + Math.round(results[0].confidence * 100) + "%";
+
+        utterThis = new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utterThis);
+    }
+
+}
